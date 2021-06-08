63751301cbe5b70f59fb1e3b8729048c01221374
Dominick Ta <domta@cs.washington.edu>
2021-06-08 11:12:02 -0700

    Add requirements.txt

--- player.py
+++ player.py
@@ -13,14 +13,16 @@ def parse(message):
     if cmd == 'play':
         play_song(get_device_id(), query)
     elif cmd == 'weather' or cmd == 'whether':
-        response = "The current weather in " + query + " is cloudy. Expect rain this afternoon."
-        transmit_speech(response)
+        hardcoded_response = "The current weather in " + query + " is cloudy. Expect rain this afternoon."
+        transmit_speech(hardcoded_response)
+    elif cmd == 'echo' or cmd == 'ego' or cmd == 'andgo' or cmd == 'andcho':
+        transmit_speech(query)
     else:
         return False
     return True
 
 async def streaming():
-    print("streaming")
+    print("Streaming!")
     async for samples in sdr.stream(fsps*Tmax):
         raw_audio = process_samples(samples, fsps, fc, Tmax, fcutoff)
         audio_play = downsample(raw_audio, fsps, faudiosps)
